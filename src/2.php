40a1c80de1f5c1fad811c32dac5496c0fae49453
nelsonmaryw86153 <nelson_maryw86153@gmx.com>
2025-03-05 16:36:01 +0000

    Add 2.php

---
+++ src/2.php
@@ -0,0 +1,14 @@
+  <?php
+$username = "johndoe";
+$password = "password123";
+$servername = "localhost";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
